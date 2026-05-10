fcbf35b55fbad67686d61441ebd373f8a255ab6b
Shuttlestepz <163724099+shuttlestepz@users.noreply.github.com>
2026-05-10 16:49:45 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -16,16 +16,17 @@ import { getFirestore }
 import { getDatabase }
   from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey           : 'AIzaSyCRwKnXko_ttD4v9WU3cWxCa8VKIzhGH5Y',
-  authDomain       : 'shuttlestepz-46f88.firebaseapp.com',
-  databaseURL      : 'https://shuttlestepz-46f88-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId        : 'shuttlestepz-46f88',
-  storageBucket    : 'shuttlestepz-46f88.firebasestorage.app',
-  messagingSenderId: '856356391368',
-  appId            : '1:856356391368:web:2712727d3ab91e60fe58fd',
-  measurementId    : 'G-SDLZ6BL4CY',
-}
+  apiKey: "AIzaSyDAznGVHeLIAR6pPSVewiw3hUpSPYtXgO4",
+  authDomain: "shuttlestepz-bac23.firebaseapp.com",
+  databaseURL: "https://shuttlestepz-bac23-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "shuttlestepz-bac23",
+  storageBucket: "shuttlestepz-bac23.firebasestorage.app",
+  messagingSenderId: "167174250097",
+  appId: "1:167174250097:web:d2b2cd1afa8818005fc5d0",
+  measurementId: "G-Y0CJ1JVD45"
+};
 
 /* Guard against double-init on hot reload / multiple imports */
 const app  = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp()
